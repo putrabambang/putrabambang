171bbf64a9f30e1459a26d2fb323726b57e96264
putrabambang <putrabambang0102@gmail.com>
2022-10-01 17:11:47 +0700

    migrate

--- database/migrations/2022_10_01_155957_tambah_column_modal_to_barang.php
+++ database/migrations/2022_10_01_155957_tambah_column_modal_to_barang.php
@@ -14,7 +14,7 @@ class TambahColumnModalToBarang extends Migration
     public function up()
     {
         Schema::table('barang', function (Blueprint $table) {
-            $table->string('modal')
+            $table->integer('modal')
             ->after('harga_jual');
         });
     }
